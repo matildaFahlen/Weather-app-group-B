724551c5c7a6fe3987034c11feb76b29c10ca460
Kevin Evenholt <kevin.evenholt@hotmail.com>
2023-02-16 20:19:03 +0100

    m

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,9 @@ const endDate = undefined;
 setUILoading();
 fetchLocation()
 	.then((locationData) => { 
+		const longitude = locationData.coords.longitude;
+		const latitude = locationData.coords.latitude;
+		fetchForecast(longitude, latitude, startDate, endDate);
 
 		if(submit) {
 			submit.addEventListener("click", () => {
@@ -15,15 +18,12 @@ fetchLocation()
 					const latitude  = data.results[0].latitude;
 					const longitude = data.results[0].longitude;
 					fetchForecast(longitude, latitude, startDate, endDate);
-					updateForeCastUI();
+					updateCurrentTempSearched(data);
 				})
 			})
 		}
-		const longitude = locationData.coords.longitude;
-		const latitude = locationData.coords.latitude;
-		fetchForecast(longitude, latitude, startDate, endDate);
 			
-	})
+	});
 	
 	// .catch((err) => {
 	// 	console.error(err);
--- javascript/searchForecast.js
+++ javascript/searchForecast.js
@@ -6,7 +6,7 @@ async function getSearchedLocation(input) {
     const data = await res.json()
     console.log( data)
     return data;
-}
+};
 
 
 
