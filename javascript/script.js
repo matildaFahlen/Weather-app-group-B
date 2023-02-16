e632b4c747a7cbf543879a268caaa668d35af2f2
Nell <eemandersson@outlook.com>
2023-02-16 13:45:04 +0100

    refactor code, creating helper functions to clean up script file

--- javascript/script.js
+++ javascript/script.js
@@ -2,17 +2,11 @@ const startDate = undefined;
 const endDate = undefined;
 
 setUILoading();
-
 fetchLocation()
 	.then((locationData) => {
 		const longitude = locationData.coords.longitude;
 		const latitude = locationData.coords.latitude;
-		fetchForecast(longitude, latitude, startDate, endDate)
-			.then(updateForeCastUI)
-			.catch((err) => {
-				console.error(err);
-				setUIError();
-			});
+		fetchForecast(longitude, latitude, startDate, endDate);
 	})
 	.catch((err) => {
 		console.error(err);
