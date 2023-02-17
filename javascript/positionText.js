4507f81f5e8884d0e3ba8ff46bbbd8f0dd32198f
SofieTiainen <tiainen77@hotmail.com>
2023-02-17 19:56:35 +0100

    position text

---
+++ javascript/positionText.js
@@ -0,0 +1,21 @@
+const locationText = document.querySelector('.current-location');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction);
+}
+
+function successFunction(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+
+    const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=sv`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const city = data['locality'];
+            const formattedCity = city.replace(' stadsdelsområde', '');
+            locationText.innerHTML = formattedCity;
+        });
+}
