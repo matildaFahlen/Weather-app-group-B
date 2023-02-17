8ec65e6bac40752333855d87e36dcb4edf6b4698
Emelie <Emelie.almlof@gmail.com>
2023-02-17 10:58:45 +0100

    fixes to checkOnlineStatus function

--- javascript/checkOnlineStatus.js
+++ javascript/checkOnlineStatus.js
@@ -1,22 +1,21 @@
 let onlineStatus = document.getElementById("online-status");
 
-//hämtar bild i assets, return false om hämtningen inte går
 const checkOnlinestatus = async () => {
     try {
-        const online = await fetch("../assets/test-pixel.png");
+        const online = await fetch("./assets/test-pixel.png");
         return online.status >= 200 && online.status < 300;
     } catch (err) {
         return false;
     }
 }
 
-//kollar var 30e sek
 setInterval(async () => {
     const result = await checkOnlinestatus();
     if (result === false) {
         console.log("offline");
-        onlineStatus.innerText = "Ingen anslutning till Internet";
+        onlineStatus.innerText = "No internet connection";
     } else {
         console.log("online")
+        onlineStatus.innerText = "";
     }
-}, 30000);
+}, 15000);
