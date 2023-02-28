812ec2cb681e292143894814468a739dcdac56a3
Matilda Fahlén <matilda_fahlen@hotmail.com>
2023-02-28 20:33:32 +0100

    moon-icon-hourly

--- javascript/updateHourlyUI.js
+++ javascript/updateHourlyUI.js
@@ -14,36 +14,50 @@ const updateHourlyUI = (forecastData) => {
 	tableBody.classList.add("div-hourly");
 
 	const startHour = new Date().getHours();
-
+	
 	let labels = [];
 	let temps = [];
-
+	
 	for (let i = startHour; i < 24 + startHour; i++) {
 		const time = forecastData.hourly.time[i];
 		const temp = forecastData.hourly.temperature_2m[i];
 		const precip = forecastData.hourly.precipitation[i];
 		const windSpeed = forecastData.hourly.windspeed_10m[i];
 		const windDirection = forecastData.hourly.winddirection_10m[i];
-
+		
 		labels.push(time.slice(-5));
 		temps.push(temp);
-
+		
 		const listElement = document.createElement("ul");
 		listElement.classList.add("hourly-forcast-ul");
-
+		
 		const hourElement = document.createElement("li");
 		hourElement.classList.add("time-hourly");
 		hourElement.innerText = `${time.slice(-5)}`;
 		listElement.append(hourElement);
-
+		
 		const iconElement = document.createElement("li");
 		iconElement.classList.add("weather-icon-hourly");
 		const iconImage = document.createElement("img");
 		iconImage.classList.add("hourly-weather-icon");
 		const weatherIcon = forecastData.hourly.weathercode[i];
-		iconImage.src = getWheatherIconUrl(weatherIcon);
-		iconElement.append(iconImage);
-		listElement.append(iconElement);
+		const sunset = forecastData.daily.sunset[0]; // To get moon-image if sun is down 
+		const sunrise = forecastData.daily.sunrise[0];
+		if (sunrise.slice(11) < hourElement.innerText && hourElement.innerText < sunset.slice(11)) { 
+			iconImage.src = getWheatherIconUrl(weatherIcon);
+			iconElement.append(iconImage);
+			listElement.append(iconElement);
+		}
+		else if (sunset.slice(11) < hourElement.innerText && weatherIcon === 0) {
+			iconImage.src = getWheatherIconUrl(-2);
+			iconElement.append(iconImage);
+			listElement.append(iconElement);
+		}
+		else {
+			iconImage.src = getWheatherIconUrl(weatherIcon);
+			iconElement.append(iconImage);
+			listElement.append(iconElement);
+		}
 
 		const tempElement = document.createElement("li");
 		tempElement.classList.add("temp-hourly");
